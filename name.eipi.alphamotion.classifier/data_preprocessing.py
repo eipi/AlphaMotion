523cfb0fa7a82095e245b044d9c5ad642501d3ba
eipi <dbdonovan@gmail.com>
2022-06-20 22:31:06 +0100

    Clean up naming and logging.

--- name.eipi.alphamotion.classifier/data_preprocessing.py
+++ name.eipi.alphamotion.classifier/data_preprocessing.py
@@ -1,3 +1,5 @@
+import sys
+
 from utilities import feature_engineer
 from constants import FEATURE_COLUMNS, DATA_BASE_PATH
 from data_access import erenaktas_target_parser
@@ -20,11 +22,12 @@ def extractSingleFile(file_type, file_num):
         print('some error')
     return dataframe.drop('target', 1)
 
-def extractAndProcessRawDataFiles(sub_path, slice_size, results_folder):
+def process_raw_data(sub_path, slice_size, results_folder):
     dataframe = pd.DataFrame(columns=FEATURE_COLUMNS)
     classification_index = erenaktas_target_parser("data/erenaktas/labels.txt")
     activity_files = os.listdir(os.path.join(DATA_BASE_PATH, sub_path))
     total_number_of_frames_in_test = 0
+    print('Processing experiments ', end=" ")
     for file in activity_files:
         filename1 = file.split('acc_exp')
         filename2 = filename1[1]
@@ -32,7 +35,7 @@ def extractAndProcessRawDataFiles(sub_path, slice_size, results_folder):
         filename4 = filename3[0]
         exp_num = int(filename4)
 
-        print("Processing experiment number " + filename4)
+        print(filename4, end = ", ")
 
         row_count = 0
         df = pd.read_csv(os.path.join(DATA_BASE_PATH, sub_path, file), sep=' ', header=None)
@@ -52,7 +55,7 @@ def extractAndProcessRawDataFiles(sub_path, slice_size, results_folder):
             sample_windows.append([row_count, row_count + num_rows_to_parse])
             row_count = row_count + num_rows_to_parse
 
-        print('* Processed ' + str(len(sample_windows)) + ' frames')
+        #print('* Processed ' + str(len(sample_windows)) + ' frames')
         total_number_of_frames_in_test = total_number_of_frames_in_test + len(sample_windows)
 
         classification_map = find_dominant_class_for_samples(target_activity_group, sample_windows)
@@ -70,7 +73,7 @@ def extractAndProcessRawDataFiles(sub_path, slice_size, results_folder):
                     )
                 else:
                     print('** Dropping 1 unclassified frame @ index ' + str(i))
-
+    print(".")
     dataframe['target'].value_counts().plot(kind='barh')
     dataframe.to_csv(results_folder + '/final_data.csv', index=False)
     return total_number_of_frames_in_test
--- name.eipi.alphamotion.classifier/experiment_main.py
+++ name.eipi.alphamotion.classifier/experiment_main.py
@@ -2,18 +2,16 @@ import time
 
 import numpy
 import pandas as pd
-import seaborn as sns
-from matplotlib import pyplot as plt
+from data_visualization import plot_and_save_confusion_matrix
 from constants import ACTIVITY_LABELS
 
 from data_classification import learn_and_classify, confusion_matrices
-from data_preprocessing import extractAndProcessRawDataFiles
+from data_preprocessing import process_raw_data
 import os
 
-num_iterations = 1
-training_proportion = 0.8
-num_samples = 61
-slice_size = 500
+num_iterations = 3
+training_proportions = [0.5]
+slice_sizes = [1000, 200]
 sample_rate_hz = 50 #todo
 
 sample_folder = os.path.join('erenaktas', 'acc')
@@ -21,44 +19,39 @@ sample_folder = os.path.join('erenaktas', 'acc')
 print("Pre-processing raw data")
 
 timestamp_str = str(int(time.time() * 1000))
-results_folder = os.path.join('../build/results/', timestamp_str)
-os.makedirs(results_folder)
-
-num_frames_processed = extractAndProcessRawDataFiles(sample_folder, slice_size, results_folder)
-df = pd.read_csv(results_folder + '/final_data.csv')
+results_base_folder = os.path.join('../build/results/', timestamp_str)
+os.makedirs(results_base_folder)
 
 
-def plot(cm, detail):
-    plt.figure(figsize=(10, 7))
-    plt.title(name)
-    sns.heatmap(cm, annot=True)
-    plt.savefig(results_folder + '/' + name + '_' + detail + '.png')
+for slice_size in slice_sizes:
+    for training_proportion in training_proportions:
+        experiment_label = 'slice_size=' + str(slice_size) + ', training_fct=' + str(training_proportion)
+        specific_results_folder = os.path.join(results_base_folder, experiment_label)
+        os.makedirs(specific_results_folder)
+        print('Running for ' + experiment_label)
+        num_frames_processed = process_raw_data(sample_folder, slice_size, specific_results_folder)
+        df = pd.read_csv(specific_results_folder + '/final_data.csv')
+        for i in range(num_iterations):
+            print('Iteration ' + str(i + 1))
+            learn_and_classify(df, training_proportion)
 
+        with open(specific_results_folder + "/Setup.txt", "w") as text_file:
+            print(f"Iterations: {num_iterations}", file=text_file)
+            print(f"Training Proportion: {training_proportion}", file=text_file)
+            print(f"Sample Rate: {sample_rate_hz}", file=text_file)
+            print(f"Slice Size: {slice_size}", file=text_file)
+            print(f"Total Number of Frames: {num_frames_processed}", file=text_file)
 
-for i in range(num_iterations):
-    learn_and_classify(df, training_proportion)
-
-
-with open(results_folder + "/Setup.txt", "w") as text_file:
-    print(f"Iterations: {num_iterations}", file=text_file)
-    print(f"Training Proportion: {training_proportion}", file=text_file)
-    print(f"Num Samples: {num_samples}", file=text_file)
-    print(f"Sample Rate: {sample_rate_hz}", file=text_file)
-    print(f"Slice Size: {slice_size}", file=text_file)
-    print(f"Total Number of Frames: {num_frames_processed}", file=text_file)
-
-
-for name in confusion_matrices.keys():
-    print(name)
-    #cm_normalized = numpy.divide(confusion_matrices[name]['normalized'], num_iterations).round(2)
-    cm_normalized = confusion_matrices[name]['normalized']
-    print(confusion_matrices[name]['natural'])
-    df_cm_natural = pd.DataFrame(confusion_matrices[name]['natural'], index=ACTIVITY_LABELS.values(),
-                         columns=ACTIVITY_LABELS.values())
-    df_cm_normalized = pd.DataFrame(cm_normalized, index=ACTIVITY_LABELS.values(),
-                         columns=ACTIVITY_LABELS.values())
-    plot(df_cm_natural, 'natural')
-    plot(df_cm_normalized, 'normalized')
-
-
+        for name in confusion_matrices.keys():
+            #print(name)
+            cm_normalized = numpy.divide(confusion_matrices[name]['normalized'], num_iterations).round(2)
+            #cm_normalized = confusion_matrices[name]['normalized']
+            #print(confusion_matrices[name]['natural'])
+            df_cm_natural = pd.DataFrame(confusion_matrices[name]['natural'], index=ACTIVITY_LABELS.values(),
+                                 columns=ACTIVITY_LABELS.values())
+            df_cm_normalized = pd.DataFrame(cm_normalized, index=ACTIVITY_LABELS.values(),
+                                 columns=ACTIVITY_LABELS.values())
+            plot_label = name + ' - ' + experiment_label
+            plot_and_save_confusion_matrix(df_cm_natural, 'natural', specific_results_folder, plot_label)
+            plot_and_save_confusion_matrix(df_cm_normalized, 'normalized', specific_results_folder, plot_label)
 

aedf8a93b2589de3de35d4a4fe74517424f5c769
eipi <dbdonovan@gmail.com>
2022-06-20 22:30:21 +0100

    Add ability to save cm to file.

--- name.eipi.alphamotion.classifier/data_visualization.py
+++ name.eipi.alphamotion.classifier/data_visualization.py
@@ -3,9 +3,23 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-perplexity=[2,5,10]
+perplexity = [2, 5, 10]
 
-def plotTsne(X, y, perplexity):
+
+def plot_and_save_confusion_matrix(cm, detail, results_folder, name):
+    plt.figure(figsize=(15, 10), dpi=100)
+    plt.title(name)
+    if detail == 'normalized':
+        s = sns.heatmap(cm, annot=True, fmt=".2f")
+    else:
+        s = sns.heatmap(cm, annot=True, fmt="d")
+    s.set_xlabel("Predicted")
+    s.set_ylabel("Actual")
+    plt.savefig(results_folder + '/' + name + '_' + detail + '.png')
+    plt.close()
+
+
+def plotTsne(X, y):
     # performing dim reduction
     X_reduce = TSNE(verbose=2, perplexity=perplexity).fit_transform(X)
 
@@ -15,11 +29,9 @@ def plotTsne(X, y, perplexity):
             'label': y}
     # preparing dataframe from reduced data
     df = pd.DataFrame(data)
-
     # draw the plot
     sns.lmplot(data=df, x='x', y='y', hue='label', fit_reg=False, height=8, \
                palette="Set1", markers=['^', 'v', 's', 'o'])
-
     plt.title("perplexity : {}".format(perplexity))
     img_name = 'TSNE_perp_{}.png'.format(perplexity)
     print('saving this plot as image in present working directory...')
